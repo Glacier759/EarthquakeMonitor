d2ac17b5855d3ebe7351f5a60f03344b4335bebb
Glacier759 <OurHom.759@gmail.com>
2015-05-03 18:27:41 +0800

    完成必应搜索模块

---
+++ src/main/java/com/glacier/earthquake/monitor/server/crawler/module/bing_search/BingDownloader.java
@@ -0,0 +1,13 @@
+package com.glacier.earthquake.monitor.server.crawler.module.bing_search;
+
+import com.glacier.earthquake.monitor.server.crawler.core.Downloader;
+import org.apache.log4j.Logger;
+
+/**
+ * Created by glacier on 15-5-3.
+ */
+public class BingDownloader extends Downloader {
+
+    public static Logger logger = Logger.getLogger(BingDownloader.class.getName());
+
+}
--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -6,6 +6,7 @@ import com.glacier.earthquake.monitor.server.crawler.Crawler;
 import com.glacier.earthquake.monitor.server.crawler.core.Downloader;
 import com.glacier.earthquake.monitor.server.crawler.module.baidu_search.BaiduSearchCrawler;
 import com.glacier.earthquake.monitor.server.crawler.module.baidu_search.BaiduSearchDownloader;
+import com.glacier.earthquake.monitor.server.crawler.module.bing_search.BingCrawler;
 import com.glacier.earthquake.monitor.server.crawler.module.tieba_search.TiebaCrawler;
 import com.glacier.earthquake.monitor.server.crawler.module.weibo_sina.WeiboCrawler;
 import com.glacier.earthquake.monitor.server.pojo.*;
@@ -28,7 +29,7 @@ public class Main {
     public static void main(String[] args) {
 
         List<Crawler> crawlers = new ArrayList<Crawler>();
-        crawlers.add(new TiebaCrawler());
+        crawlers.add(new BingCrawler());
         for ( Crawler crawler : crawlers ) {
             crawler.start();
         }
