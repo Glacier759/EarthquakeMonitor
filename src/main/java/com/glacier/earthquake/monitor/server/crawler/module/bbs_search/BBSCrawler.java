984b53919d96f814e8d8a7cee6af7c86821ef027
Glacier759 <OurHom.759@gmail.com>
2015-05-03 18:02:28 +0800

    开发BBS模块，感觉比较麻烦的样子..  正在设计通用的可配置搜索过滤模块

---
+++ src/main/java/com/glacier/earthquake/monitor/server/crawler/module/bbs_search/BBSCrawler.java
@@ -0,0 +1,14 @@
+package com.glacier.earthquake.monitor.server.crawler.module.bbs_search;
+
+import com.glacier.earthquake.monitor.server.crawler.Crawler;
+
+/**
+ * Created by glacier on 15-5-3.
+ */
+public class BBSCrawler extends Crawler {
+
+    @Override
+    public void start() {
+
+    }
+}
