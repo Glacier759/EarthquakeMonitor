8da7d6f97cc42047f3249967f8e800d5b1f98147
Glacier759 <OurHom.759@gmail.com>
2015-05-22 18:31:38 +0800

    完成文件上传功能，用于用户规则的批量导入

---
+++ src/main/java/com/glacier/earthquake/monitor/browser/servlet/UploadServlet.java
@@ -0,0 +1,72 @@
+package com.glacier.earthquake.monitor.browser.servlet;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+
+/**
+ * Created by glacier on 15-5-22.
+ */
+public class UploadServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("index.jsp");
+
+        //判断提交过来的表单是否为文件上传菜单
+        boolean isMultipart = ServletFileUpload.isMultipartContent(request);
+        if (isMultipart) {
+            //构造一个文件上传处理对象
+            FileItemFactory factory = new DiskFileItemFactory();
+            ServletFileUpload upload = new ServletFileUpload(factory);
+            Iterator items;
+            try {
+                //解析表单中提交的所有文件内容
+                items = upload.parseRequest(request).iterator();
+
+                while (items.hasNext()) {
+                    FileItem item = (FileItem) items.next();
+                    if (!item.isFormField()) {
+                        //取出上传文件的文件名称
+                        String name = item.getName();
+                        //取得上传文件以后的存储路径
+                        String fileName = name.substring(name.lastIndexOf('\\') + 1, name.length());
+                        //上传文件以后的存储路径
+                        String path = "./tmp/download_file/" + File.separatorChar;
+                        File path_dir = new File(path);
+                        if ( !path_dir.exists() ) {
+                            path_dir.mkdirs();
+                        }
+                        //上传文件
+                        File uploaderFile = new File(path + fileName);
+                        if ( !uploaderFile.exists() ) {
+                            uploaderFile.createNewFile();
+                        }
+                        System.out.println(uploaderFile.getAbsolutePath());
+                        item.write(uploaderFile);
+                        System.out.println("文件写入完毕");
+
+                        System.out.println("文件删除完毕 - " + uploaderFile.delete());
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        requestDispatcher.forward(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+}
--- src/main/webapp/submit.jsp
+++ src/main/webapp/submit.jsp
@@ -1,12 +1,4 @@
 <%@ page contentType="text/html; charset=UTF-8" %>
-<%
-    if ( session.getAttribute("login") == null || session.getAttribute("login").equals("false") ) {
-        response.sendRedirect(request.getContextPath() + "/nogrant.jsp");
-    }
-    if ( request.getParameter("groupID") == null ) {
-        response.sendRedirect(request.getContextPath() + "/select.jsp");
-    }
-%>
 <html>
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
@@ -43,13 +35,6 @@
 <div id="content">
     <div id="submit">
         <form id="submitF" action="<%=request.getContextPath()%>/UploadServlet" method="post" enctype="multipart/form-data">
-            <%
-                String groupID = request.getParameter("groupID");
-                if ( groupID == null ) {
-                    response.sendRedirect(request.getContextPath() + "/select.jsp");
-                }
-                session.setAttribute("groupID", new Integer(groupID));
-            %>
             <label for="file">文件名:</label>
             <input class="submit" type="file" name="file" id="file" />
             <br />
